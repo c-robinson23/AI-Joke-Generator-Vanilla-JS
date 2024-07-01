b97f6e6fab440f737d32cc04b5c3202673d8eee3
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-07-01 15:56:45 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ function handleClick(event) {
   event.preventDefault();
   let apiKey = "8955aec3fa84eee934db0tdod70f6d28";
   let context =
-    "You are a great AI assistant that gives many funny, appropriate jokes. Please answer concise and clear with no inapropriate jokes. Do not repeat the previous joke generated.";
-  let prompt = "Generate short and funny computer-type jokes.";
+    "You are a great AI assistant that gives various funny, appropriate jokes. Please answer concise and clear. No inapropriate jokes. Generate a new nerd joke every time the button is clicked. Generate a new nerd joke with a API fetch response.";
+  let prompt = "Generate a new short and funny nerd joke.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   joke.innerHTML = "Generating a joke for you...please wait";
